b9c9753aab0f2813f1611685207836013c777728
Narimm <benjicharlton@gmail.com>
2018-03-12 08:08:26 +1000

    Improve travis build - cache m2

--- prepareEnvironment.sh
+++ prepareEnvironment.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-java -jar BuildTools.jar
+if [ -f builder/Spigot.jar]; then
+    wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar --rev 1.12.2
+    java -jar BuildTools.jar
+    mv Spigot.jar builder/Spigot.jar
+else
+    mvn install:install-file -DgroupId=org.spigotmc -DartifactId=Spigot -Dversion=1.12.2-R0.1-SNAPSHOT -Dfile=builder/Spigot.jar
+fi
