428542e1c9b9400aef4038a720ad61ca98393eb7
Narimm <benjicharlton@gmail.com>
2018-03-12 08:22:43 +1000

    remove conditional

--- prepareEnvironment.sh
+++ prepareEnvironment.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-if [ -f builder/Spigot.jar ]
-then
-    wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar --rev 1.12.2
-    java -jar BuildTools.jar
-    mv Spigot.jar builder/Spigot.jar
-else
-    mvn install:install-file -DgroupId=org.spigotmc -DartifactId=Spigot -Dversion=1.12.2-R0.1-SNAPSHOT -Dfile=builder/Spigot.jar
-fi
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar --rev 1.12.2
+java -jar BuildTools.jar
