1c9d0327890684f6718bbcc8e34bba275d3faeaa
Narimm <benjicharlton@gmail.com>
2018-03-13 15:23:40 +1000

    fix script

--- prepareEnvironment.sh
+++ prepareEnvironment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ev
-if ! [[ -f builder/Spigot.jar ]]; then
+if ! [[ -f builder/Spigot-${SPIGOT_VERSION}.jar ]]; then
     wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
     java -jar BuildTools.jar --rev ${SPIGOT_VERSION}
-    cp Spigot.jar builder/Spigot.jar
+    cp Spigot*.jar builder/Spigot*.jar
 fi
