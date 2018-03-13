294c558108c48eb248f67fb5392aa6c28fa3421f
Narimm <benjicharlton@gmail.com>
2018-03-13 15:44:22 +1000

    pipe buildtools to null

--- prepareEnvironment.sh
+++ prepareEnvironment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ev
-if ! [[ -f builder/Spigot-${SPIGOT_VERSION}.jar ]]; then
+if ! [[ -f builder/spigot-${SPIGOT_VERSION}.jar ]]; then
     wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-    java -jar BuildTools.jar --rev ${SPIGOT_VERSION}
-    cp Spigot*.jar builder/Spigot*.jar
+    java -jar BuildTools.jar --rev ${SPIGOT_VERSION} > /dev/null
+    cp spigot*.jar builder/spigot*.jar
 fi
